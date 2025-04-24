dae4ebc2b16c603c21bfbd965ac0833225343dc4
Anoosh_S <anoosh.sankuru@capillarytech.com>
2025-04-24 13:05:14 +0530

    added virtual scrolling

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,16 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, ScrollingModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-app';
   count = signal(1);
-
+  // items = Array.from({ length: 1000 }, (_, i) => i + 1);
+  items = Array.from({ length: 1000 }).map((i, j) => ({
+    id: j,
+    name: `Item ${j}`,
+  }));
+  constructor() {
+    console.log('count', this.count());
+    console.log('items', this.items);
+  }
   increment() {
     this.count.update((v) => v + 1);
   }
